20dbff520af18eff7c94eac355489a1e86f387da
Egor <egormalov.2007@gmail.com>
2025-08-15 18:30:20 +0300

    №02. exception, mapper package added, pom has been modified.

---
+++ src/main/java/ru/stroy1click/userservice/exception/ValidationException.java
@@ -0,0 +1,9 @@
+package ru.stroy1click.userservice.exception;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+}
