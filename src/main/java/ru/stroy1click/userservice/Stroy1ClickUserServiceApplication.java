185518e9c4a80db673701619033f6d252f586ed9
Egor Malov <egormalov@MacBook-Air-Egor.local>
2025-08-15 17:42:28 +0300

    Initial commit

---
+++ src/main/java/ru/stroy1click/userservice/Stroy1ClickUserServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.stroy1click.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stroy1ClickUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stroy1ClickUserServiceApplication.class, args);
+    }
+
+}
