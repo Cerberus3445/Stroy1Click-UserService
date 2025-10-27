189bc775724543b5f24ee074ee57976a8e01e7d9
Egor <egormalov.2007@gmail.com>
2025-10-27 22:04:10 +0300

    №09. Tests refactoring

--- src/test/java/ru/stroy1click/user/unit/UserUnitTest.java
+++ src/test/java/ru/stroy1click/user/unit/UserUnitTest.java
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class UserUnitTests {
+public class UserUnitTest {
 
     @Mock
     private UserRepository userRepository;
